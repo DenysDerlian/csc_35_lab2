e8fc414a2f8d069db40df602b80dbf738a348777
Denys Derlian <denysderlian@hotmail.com>
2024-09-27 20:48:50 -0300

    up lab3 filter 3

--- AppFilterL3.py
+++ AppFilterL3.py
@@ -21,6 +21,8 @@ from ryu.lib.packet import packet
 from ryu.lib.packet import ethernet
 from ryu.lib.packet import ether_types
 from ryu.lib.packet import ipv4
+from ryu.lib.packet import arp
+
 
 class L3Filter(app_manager.RyuApp):
     OFP_VERSIONS = [ofproto_v1_3.OFP_VERSION]
@@ -41,9 +43,10 @@ class L3Filter(app_manager.RyuApp):
         ofproto = datapath.ofproto
         parser = datapath.ofproto_parser
 
-        # Instalar entrada de fluxo table-miss que descarta pacotes por padrão
+        # Instalar entrada de fluxo table-miss que envia pacotes para o controlador
         match = parser.OFPMatch()
-        actions = []  # Sem ações significa que o pacote será descartado
+        actions = [parser.OFPActionOutput(ofproto.OFPP_CONTROLLER,
+                                          ofproto.OFPCML_NO_BUFFER)]
         self.add_flow(datapath, 0, match, actions)
 
     def add_flow(self, datapath, priority, match, actions, buffer_id=None):
@@ -72,47 +75,108 @@ class L3Filter(app_manager.RyuApp):
         pkt = packet.Packet(msg.data)
         eth = pkt.get_protocols(ethernet.ethernet)[0]
 
+        # Log do tipo de pacote recebido
+        self.logger.info("Pacote recebido: eth_type=0x%04x", eth.ethertype)
+
         if eth.ethertype == ether_types.ETH_TYPE_LLDP:
             # Ignorar pacotes LLDP
             return
 
-        ip_pkt = pkt.get_protocol(ipv4.ipv4)
-        if ip_pkt:
-            src_ip = ip_pkt.src
-            dst_ip = ip_pkt.dst
+        # Manipular pacotes ARP
+        if eth.ethertype == ether_types.ETH_TYPE_ARP:
+            arp_pkt = pkt.get_protocol(arp.arp)
+            if arp_pkt.opcode == arp.ARP_REQUEST or arp_pkt.opcode == arp.ARP_REPLY:
+                self.logger.info("Processando pacote ARP: %s -> %s", arp_pkt.src_ip, arp_pkt.dst_ip)
+                self.handle_arp(datapath, in_port, eth, arp_pkt)
+            return  # Após processar ARP, não precisa continuar
 
-            # Verificar se o par de IPs está na lista de permitidos
-            if (src_ip, dst_ip) in self.ALLOWED_IP_PAIRS:
-                self.logger.info("Permitido: %s -> %s", src_ip, dst_ip)
+        # Manipular pacotes IPv4
+        if eth.ethertype == ether_types.ETH_TYPE_IP:
+            ip_pkt = pkt.get_protocol(ipv4.ipv4)
+            if ip_pkt:
+                src_ip = ip_pkt.src
+                dst_ip = ip_pkt.dst
 
-                # Aprender o mapeamento MAC -> Porta
-                src = eth.src
-                dst = eth.dst
-                dpid = format(datapath.id, "d").zfill(16)
-                self.mac_to_port.setdefault(dpid, {})
-                self.mac_to_port[dpid][src] = in_port
+                # Verificar se o par de IPs está na lista de permitidos
+                if (src_ip, dst_ip) in self.ALLOWED_IP_PAIRS:
+                    self.logger.info("Permitido: %s -> %s", src_ip, dst_ip)
 
-                if dst in self.mac_to_port[dpid]:
-                    out_port = self.mac_to_port[dpid][dst]
+                    # Aprender o mapeamento MAC -> Porta
+                    src = eth.src
+                    dst = eth.dst
+                    dpid = format(datapath.id, "d").zfill(16)
+                    self.mac_to_port.setdefault(dpid, {})
+                    self.mac_to_port[dpid][src] = in_port
+
+                    if dst in self.mac_to_port[dpid]:
+                        out_port = self.mac_to_port[dpid][dst]
+                    else:
+                        out_port = ofproto.OFPP_FLOOD
+
+                    actions = [parser.OFPActionOutput(out_port)]
+
+                    # Instalar fluxo para permitir tráfego futuro entre h1 e h2
+                    match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP,
+                                            ipv4_src=src_ip, ipv4_dst=dst_ip)
+                    self.add_flow(datapath, 1, match, actions, msg.buffer_id)
+
+                    # Enviar o pacote para a porta de saída
+                    out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
+                                              in_port=in_port, actions=actions, data=msg.data)
+                    datapath.send_msg(out)
+                    return
                 else:
-                    out_port = ofproto.OFPP_FLOOD
+                    self.logger.info("Bloqueado: %s -> %s", src_ip, dst_ip)
+                    # Não instalar fluxo, descartando o pacote
+                    return
 
-                actions = [parser.OFPActionOutput(out_port)]
+        # Para pacotes que não são ARP ou IP, descartar
+        self.logger.info("Pacote não IP e não ARP descartado.")
+        return
 
-                # Instalar fluxo para permitir tráfego futuro entre h1 e h2
-                match = parser.OFPMatch(in_port=in_port, eth_type=ether_types.ETH_TYPE_IP,
-                                        ipv4_src=src_ip, ipv4_dst=dst_ip)
-                self.add_flow(datapath, 1, match, actions)
-                
-                # Enviar o pacote para a porta de saída
-                out = parser.OFPPacketOut(datapath=datapath, buffer_id=msg.buffer_id,
-                                          in_port=in_port, actions=actions, data=msg.data)
-                datapath.send_msg(out)
-                return
-            else:
-                self.logger.info("Bloqueado: %s -> %s", src_ip, dst_ip)
-                # Não instalar fluxo, descartando o pacote
-                return
-        else:
-            # Para pacotes que não são IP, descartar
-            return
+    def handle_arp(self, datapath, in_port, eth, arp_pkt):
+        """
+        Manipula pacotes ARP para permitir a comunicação entre h1 e h2.
+        """
+        parser = datapath.ofproto_parser
+        ofproto = datapath.ofproto
+
+        # Aprender o mapeamento MAC -> Porta para ARP
+        src_mac = arp_pkt.src_mac
+        src_ip = arp_pkt.src_ip
+        self.mac_to_port.setdefault(format(datapath.id, "d").zfill(16), {})
+        self.mac_to_port[format(datapath.id, "d").zfill(16)][src_mac] = in_port
+
+        # Criar correspondência para responder ao ARP
+        if arp_pkt.opcode == arp.ARP_REQUEST:
+            # Verificar se o ARP é para h1 ou h2
+            if arp_pkt.dst_ip in ['10.0.0.1', '10.0.0.2']:
+                # Encontrar o MAC correspondente ao dst_ip
+                dst_mac = None
+                for mac, port in self.mac_to_port[format(datapath.id, "d").zfill(16)].items():
+                    if port == in_port and mac != src_mac:
+                        dst_mac = mac
+                        break
+                if dst_mac:
+                    # Construir pacote ARP Reply
+                    arp_reply = packet.Packet()
+                    arp_reply.add_protocol(ethernet.ethernet(
+                        ethertype=ether_types.ETH_TYPE_ARP,
+                        src=eth.dst,
+                        dst=eth.src
+                    ))
+                    arp_reply.add_protocol(arp.arp(
+                        opcode=arp.ARP_REPLY,
+                        src_mac=eth.dst,
+                        src_ip=arp_pkt.dst_ip,
+                        dst_mac=eth.src,
+                        dst_ip=arp_pkt.src_ip
+                    ))
+                    arp_reply.serialize()
+
+                    actions = [parser.OFPActionOutput(in_port)]
+                    out = parser.OFPPacketOut(datapath=datapath, buffer_id=ofproto.OFP_NO_BUFFER,
+                                              in_port=ofproto.OFPP_CONTROLLER,
+                                              actions=actions, data=arp_reply.data)
+                    datapath.send_msg(out)
+                    self.logger.info("Enviado ARP Reply: %s -> %s", arp_pkt.dst_ip, arp_pkt.src_ip)
